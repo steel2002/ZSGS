0fa88e3260cce5b52df0c14086a40f9a5dd83f01
SELVIN R <steelselvin2002@gmail.com>
2025-08-07 16:39:42 +0530

    DAY 8

--- DAY8.java
+++ DAY8.java
@@ -4,7 +4,11 @@
 
 // 1. Illustrates with an example of using Singleton class.
 
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 class Singleton {
 
     private static Singleton instance;
@@ -38,3 +42,113 @@ public class DAY8 {
 }
 
 // 2. Develop a Java program which illustrates the usage of Comparator Interface.
+
+class Student
+{
+	String name;
+	int age;
+
+	public Student(String name, int age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+
+	public String toString()
+	{
+		return "Student [ Name: "+this.name+" ,Age: "+this.age+" ]";
+	}
+}
+
+class Demo
+{
+	public static void main(String[]args)
+	{
+		Comparator<Student>cmp = new Comparator<Student>()
+		{
+			public int compare(Student i, Student j)
+			{
+				return i.age - j.age;                            
+			}
+		};
+
+		List<Student> studs = new ArrayList<Student>();
+		studs.add(new Student("java", 25));
+		studs.add(new Student("python", 15));
+		studs.add(new Student("javascript", 24));
+		Collections.sort(studs, cmp);
+			
+		for(Student s: studs)
+		{
+			System.out.println(s);
+		}
+	}
+}
+
+// 3. Develop a Java program which illustrates the usage of Comparable Interface.
+
+class Student1 implements Comparable<Student1>
+{
+	String name;
+	int age;
+
+	public Student1(String name, int age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+
+	public String toString()
+	{
+		return "Student [ Name: "+this.name+" ,Age: "+this.age+" ]";
+	}
+	
+	public int compareTo(Student1 that)
+	{
+		return this.age - that.age;
+	}
+}
+
+class Demo1
+{
+	public static void main(String[]args)
+	{
+
+		List<Student1> studs = new ArrayList<Student1>();
+		studs.add(new Student1("java", 25));
+		studs.add(new Student1("python", 15));
+		studs.add(new Student1("sql", 24));
+
+		Collections.sort(studs);
+	
+		for(Student1 s: studs)
+		{
+			System.out.println(s);
+		}
+	}
+}
+
+
+
+
+
+
+class Main{
+    public static void main(String[] args) {
+
+
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
