2afe4fa7410bbf638351117c63107338c37a4ba8
SELVIN R <steelselvin2002@gmail.com>
2025-08-09 14:32:38 +0530

    Time 2.30

--- DAY8.java
+++ DAY8.java
@@ -219,7 +219,94 @@ class CallByValue {
 }
 // 7. Develop a Java program to illustrate the usage of toString() method.
 
+class Car {
+	String brand;
+	String color;
+	 int model;
 
+	 Car(String brand,String color ,int model){
+		this.brand=brand;
+		this.color=color;
+		this.model=model;
+	 }
+
+public String toString(){
+
+	return "Brand :" +brand+"\n Color :" +color+ "\n Model:" +model ;
+}
+
+}
+class Vehicle{
+public static void main(String [] args){
+
+	Car c = new Car("BMW", "RED", 2025);
+	System.out.println(" " +c.toString());
+}
+}
+
+// 8. Write a Java program to demonstrate the concept of object cloning using the clone() method.
+// --->Create a class Student with fields like name, rollNo, and department.
+// --->Attempt to clone an object of this class using the clone() method.
+// --->Catch and handle the CloneNotSupportedException if thrown.
+// ***Also find out and explain why the class must implement the Cloneable interface to avoid CloneNotSupportedException.
+// -->Your program should clearly illustrate:
+// a. What happens if Cloneable is not implemented
+// b. How the error is resolved by implementing Cloneable
+
+class Student10{
+	int rollNo;
+	String name;
+	String department;
+
+	Student10(int rollNo,String name,String department){
+		this.rollNo=rollNo;
+		this.name=name;
+		this.department=department;
+	}
+}
+class Person10 implements Cloneable {
+	String city;
+	Student10 s;
+
+	Person10(String city,Student10 s){
+		this.city=city;
+		this.s=s;
+	}
+
+	public String toString(){
+		return "Name :"+s.name+"\n RollNumber :"+s.rollNo+ "\nDepart :"+s.department+"\n city :" + city;
+	}
+
+protected Object clone() throws CloneNotSupportedException{
+
+	return super.clone();
+
+}
+
+}
+
+class StudentSystem{
+	public static void main(String[] args) {
+		
+		try {
+
+			Student10 s = new Student10(666,"selvin","eee");
+			Person10 p = new Person10("Chennai",s);
+			Person10 p1 = (Person10) p.clone();
+               System.out.println(" " +p);
+			System.out.println(" " +p1);
+			p.s.rollNo=555;
+			System.out.println();
+
+			System.out.println("After change Value :"+ p);
+			System.out.println(" After Change Value  :" +p1);
+			
+		} catch (Exception e) {
+			System.out.println(" " +e.getLocalizedMessage());
+			
+		}
+	}
+}
 
 
 
