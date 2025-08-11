09385024d970612cea09e507ed386eefa3ee9034
SELVIN R <steelselvin2002@gmail.com>
2025-08-11 12:19:52 +0530

    11.08 am

---
+++ DAY12.java
@@ -0,0 +1,103 @@
+public class DAY12 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
+// *Day-12 Assignment Questions:*
+
+// 1. Write a Java program to demonstrate multithreading by extending the Thread class.
+// --->Create a class MyThread that extends Thread.
+// --->Override the run() method to display the thread name and a message five times with a delay of 500 milliseconds between prints.
+// --->In the main() method, create two objects of MyThread and start them.
+// --->Each thread prints its message independently, showing concurrent execution.
+
+
+class MyThread extends Thread {
+
+    public void run(){
+   
+        for(int i=0;i<5;i++){
+
+            System.out.println(Thread.currentThread().getName() + " Notify");
+
+           try {
+                
+            Thread.sleep(500);
+             
+           } catch (InterruptedException e) {
+
+               System.out.println(" " + e.getMessage());
+            
+           }
+        }
+    }
+
+
+    public static void main(String[] args) {
+       MyThread t1 = new MyThread();
+       MyThread t2 = new MyThread();
+       MyThread t3 = new MyThread();
+
+        t1.setName("A");
+        t2.setName("B");
+        t3.setName("C");
+
+        t1.start();
+        t2.start();
+        t3.start();
+          
+    }
+}
+
+// 2. Write a Java program to create a thread using the Runnable interface.
+// --->Create a class TaskRunner that implements Runnable.
+// --->Inside the run() method, print the current thread name and a task-specific message 10 times with a delay of 1000ms.
+// --->In the main() method, create two Thread objects passing different TaskRunner instances and start both threads.
+// --->The console should reflect the concurrent execution of both tasks.
+
+class TaskRunner implements Runnable {
+
+    public void run(){
+
+        for(int i=0;i<10;i++){
+
+           System.out.println(Thread.currentThread().getName() + " Notify :");
+
+
+           try {
+              
+            Thread.sleep(1000);
+
+           } catch(InterruptedException e){
+
+            System.out.println(" " + e.getMessage());
+
+           }
+
+        }
+    }
+
+
+public static void main(String[] args) {
+
+    TaskRunner t1 = new TaskRunner();
+    TaskRunner t2 = new TaskRunner();
+
+    Thread t3 = new Thread(t1);
+    Thread t4 = new Thread (t2);
+
+      t3.setName(" A ");
+      t4.setName(" B ");
+      
+      t3.start();
+      t4.start();
+
+        
+    }
+
+}
+
+// 3. Write a Java program where one thread prints only even numbers and another prints only odd numbers from 1 to 20. 
+// Synchronize the threads so that they print alternately (i.e., 1 2 3 4 ...)
